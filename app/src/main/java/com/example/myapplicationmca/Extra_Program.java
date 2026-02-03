f0175bbfdb55de5329733268575aa631c7e2278d
Students <student@mefgi.com>
2026-02-03 12:05:42 +0530

    03-02-2026

--- app/src/main/java/com/example/myapplicationmca/Extra_Program.java
+++ app/src/main/java/com/example/myapplicationmca/Extra_Program.java
@@ -48,7 +48,8 @@ public class Extra_Program extends AppCompatActivity {
         pro4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Extra_Program.this, "NOT MAKE", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(Extra_Program.this, E_pro4.class);
+                startActivity(i);
             }
         });
         pro5.setOnClickListener(new View.OnClickListener() {
