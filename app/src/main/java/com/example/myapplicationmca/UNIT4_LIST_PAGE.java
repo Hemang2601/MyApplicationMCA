a35e454419dd7fdd17db42ba7a1a25ed2ceafcb3
Students <student@mefgi.com>
2026-03-03 12:01:23 +0530

    done Unit 4

--- app/src/main/java/com/example/myapplicationmca/UNIT4_LIST_PAGE.java
+++ app/src/main/java/com/example/myapplicationmca/UNIT4_LIST_PAGE.java
@@ -1,57 +1,84 @@
 package com.example.myapplicationmca;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class UNIT4_LIST_PAGE extends AppCompatActivity {
 
-    Button pro1,pro2,pro3,pro4,pro5;
+    MaterialCardView pro44, pro45, pro46, pro47, pro48, pro49, pro50;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unit4_list_page);
-        pro1 = findViewById(R.id.btnpro1);
-        pro2 = findViewById(R.id.btnpro2);
-        pro3 = findViewById(R.id.btnpro3);
-        pro4 = findViewById(R.id.btnpro4);
-        pro5 = findViewById(R.id.btnpro5);
 
-        pro1.setOnClickListener(new View.OnClickListener() {
+        pro44 = findViewById(R.id.btnPro44);
+        pro45 = findViewById(R.id.btnPro45);
+        pro46 = findViewById(R.id.btnPro46);
+        pro47 = findViewById(R.id.btnPro47);
+        pro48 = findViewById(R.id.btnPro48);
+        pro49 = findViewById(R.id.btnPro49);
+        pro50 = findViewById(R.id.btnPro50);
+
+        pro44.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(UNIT4_LIST_PAGE.this, "NOT MAKE", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(UNIT4_LIST_PAGE.this, UNIT4_pro44.class);
+                startActivity(i);
             }
         });
-        pro2.setOnClickListener(new View.OnClickListener() {
+
+        pro45.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(UNIT4_LIST_PAGE.this, "NOT MAKE", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(UNIT4_LIST_PAGE.this, UNIT4_pro45.class);
+                startActivity(i);
             }
         });
-        pro3.setOnClickListener(new View.OnClickListener() {
+
+        pro46.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(UNIT4_LIST_PAGE.this, "NOT MAKE", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(UNIT4_LIST_PAGE.this, UNIT4_pro46.class);
+                startActivity(i);
             }
         });
-        pro4.setOnClickListener(new View.OnClickListener() {
+
+        pro47.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(UNIT4_LIST_PAGE.this, "NOT MAKE", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(UNIT4_LIST_PAGE.this, UNIT4_pro47.class);
+                startActivity(i);
             }
         });
-        pro5.setOnClickListener(new View.OnClickListener() {
+
+        pro48.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(UNIT4_LIST_PAGE.this, "NOT MAKE", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(UNIT4_LIST_PAGE.this, UNIT4_pro48.class);
+                startActivity(i);
+            }
+        });
+
+        pro49.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(UNIT4_LIST_PAGE.this, UNIT4_pro49.class);
+                startActivity(i);
+            }
+        });
+
+        pro50.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(UNIT4_LIST_PAGE.this, UNIT4_pro50.class);
+                startActivity(i);
             }
         });
 
